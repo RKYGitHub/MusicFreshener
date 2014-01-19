82e440acfabd42a670a842b30451c89a5f326aa6
RKYGitHub <robert.k.yates@gmail.com>
2014-01-19 16:43:57 -0500

    Initial commit Just a basic drawer so far, nothing more.

---
+++ MusicFreshener/src/main/java/com/rky/musicfreshener/CsvUtilities.java
@@ -0,0 +1,21 @@
+package com.rky.musicfreshener;
+
+import android.content.Context;
+import android.database.MatrixCursor;
+import android.widget.CursorAdapter;
+import android.widget.SimpleCursorAdapter;
+
+/**
+ * Created by Robbie on 11/3/13.
+ */
+public class CsvUtilities {
+/*
+    static CursorAdapter getAdapter(Context context) {
+        MatrixCursor matrix = new MatrixCursor(new String[]{"col1", "col2", "col3", "col4", "col5", "col6"});
+        matrix.addRow(new String[]{"val11", "val12", "val13", "val14", "val15", "val16"});
+        matrix.addRow(new String[]{"val21", "val22", "val23", "val24", "val25", "val26"});
+
+
+    }
+*/
+}
